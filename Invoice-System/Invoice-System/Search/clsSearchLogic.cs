dae9c62dd067699a64d5cf7216cfe11c50d49ce4
Jesse Tice <jessetice@mail.weber.edu>
2020-11-24 21:36:07 -0700

    Update clsSearchLogic.cs
    Added some more methods.

--- Invoice-System/Invoice-System/Search/clsSearchLogic.cs
+++ Invoice-System/Invoice-System/Search/clsSearchLogic.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -7,89 +7,146 @@ using System.Threading.Tasks;
 
 namespace Invoice_System.Search
 {
-
-    /// <summary>
-    /// Class that handles the logic
-    /// </summary>
     public class clsSearchLogic
     {
         /// <summary>
-        /// Used to query the db
+        /// clsSearchSQL object used to pass values that are needed to query Invoice database.
         /// </summary>
-        private clsSearchSQL clsSQLManager;
+        private clsSearchSQL clsSearchSQLManager;
 
         /// <summary>
-        /// Invoice number
+        /// integer used to store a specific InvoiceNum value
         /// </summary>
         public int iInvoiceNum;
 
         /// <summary>
-        /// Invoice date
+        /// DateTime used to store a specific InvoiceDate value
         /// </summary>
         public DateTime dateInvoiceDate;
 
         /// <summary>
-        /// Invoice cost
+        /// integer used to store a specific TotalCost value
         /// </summary>
         public int iTotalCost;
 
+        /// <summary>
+        /// IList of clsSearchLogic objects that represent a row in the Invoice table.
+        /// </summary>
+        public IList<clsSearchLogic> clsSearchLogicList;
 
         /// <summary>
-        /// Default Constructor
+        /// clsSearchLogic Constructor initializing the clsSearchSQLManager object.
         /// </summary>
         public clsSearchLogic()
         {
-            clsSQLManager = new clsSearchSQL();
+            clsSearchSQLManager = new clsSearchSQL();
         }
 
         /// <summary>
-        /// Overloaded Constructor
+        /// clsSearchLogic Constructor initializing the iInvoiceNum, dateInvoiceDate, and iTotalCost values for a specific Invoice.
         /// </summary>
-        /// <param name="invoiceNum"></param>
-        /// <param name="invoiceDate"></param>
-        /// <param name="totalCost"></param>
-        public clsSearchLogic(int invoiceNum, DateTime invoiceDate, int totalCost)
+        /// <param name="tempInvoiceNum"></param>
+        /// <param name="tempInvoiceDate"></param>
+        /// <param name="tempTotalCost"></param>
+        public clsSearchLogic(int tempInvoiceNum, DateTime tempInvoiceDate, int tempTotalCost)
         {
-            iInvoiceNum = invoiceNum;
-            dateInvoiceDate = invoiceDate;
-            iTotalCost = totalCost;
+            // This method sets the clsSearchLogic object's iInvoiceNum, dateInvoiceDate, and iTotalCost on creation of a new clsSearchLogic object.
+            iInvoiceNum = tempInvoiceNum;
+            dateInvoiceDate = tempInvoiceDate;
+            iTotalCost = tempTotalCost;
         }
 
         /// <summary>
-        /// Setter used to add query to search window
+        /// Method to reset values that will be used in the Search window to populate datagrid and comboboxes.
         /// </summary>
-        /// <param name="invoiceNum"></param>
-        public void queryInvoiceNum(int invoiceNum)
+        public void refreshValues()
         {
-            ///TODO
+            try
+            {
+                // Uses the clsSearchSQL object to query the whole Invoice table and return a dataset.
+                // Uses the dataset to loop through and create a new clsSearchLogic object using the overloaded operator to set its attributes.
+                // While looping through and creating objects, each object is added to a new clsSearchLogicList (IList<clsSearchLogic>) which is used to populate the Search window.
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                       MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
         }
 
         /// <summary>
-        /// Query the db for date and total cost and populates the search window
+        /// This method accepts an InvoiceNum which is used by the clsSearchSQL object to set the iInvoiceNum, dateInvoiceDate, and iTotalCost.
         /// </summary>
-        /// <param name="invoiceDate"></param>
-        /// <param name="totalCost"></param>
-        public void queryInvoiceDateCost(DateTime invoiceDate, int totalCost)
+        /// <param name="tempInvoiceNum"></param>
+        public void queryInvoiceNum(int tempInvoiceNum)
         {
-            ///TODO
+            try
+            {
+                // This method will use the clsSearchSQL object's querySpecificInvoiceNum(int tempInvoiceNum) method and store it's returned Invoice in a dataset
+                // This method will then use the dataset to create a new clsSearchLogicList and adding to it the clsSearchLogic object
+                // This clsSearchLogic object contains the values of the Invoice that is in the dataset.
+                // This will set the needed values to populate the Search window.
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                       MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
         }
 
         /// <summary>
-        /// Query the db for total cost and populate the search window
-        /// </summary>
-        /// <param name="totalCost"></param>
-        public void queryInvoiceCost(int totalCost)
-        {
-            ///TODO
-        }
-
-        /// <summary>
-        /// Query the db for date and populate the search window
+        /// Uses the clsSearchSQL object to query and return a dataset to create a new List used to populate the Search window.
         /// </summary>
         /// <param name="tempInvoiceDate"></param>
-        public void queryInvoiceDate(DateTime tempInvoiceDate)
+        /// <param name="tempTotalCost"></param>
+        public void queryInvoices(DateTime tempInvoiceDate, int tempTotalCost)
         {
-            ///TODO
+            try
+            {
+                // This method will use the clsSearchSQL object's queryByDateAndCost method to retrieve a dataset and create a new clsSearchLogicList.
+                // This List is used to populate the Search window.
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                       MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Uses the clsSearchSQL object to query and return a dataset to create a new List used to populate the Search window.
+        /// </summary>
+        /// <param name="tempTotalCost"></param>
+        public void queryInvoices(int tempTotalCost)
+        {
+            try
+            {
+                // This method will use the clsSearchSQL object's queryByCost method to retrieve a dataset and create a new clsSearchLogicList.
+                // This List is used to populate the Search window.
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                       MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Uses the clsSearchSQL object to query and return a dataset to create a new List used to populate the Search window.
+        /// </summary>
+        /// <param name="tempInvoiceDate"></param>
+        public void queryInvoices(DateTime tempInvoiceDate)
+        {
+            try
+            {
+                // This method will use the clsSearchSQL object's queryByDate method to retrieve a dataset and create a new clsSearchLogicList.
+                // This List is used to populate the Search window.
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                       MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
         }
     }
 }
